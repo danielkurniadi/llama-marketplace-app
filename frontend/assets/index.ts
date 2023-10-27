85d7bcf3568d1b718be376a0d0a71e6d6e010a54
iqdf <daniel.thekurniadi@gmail.com>
2023-10-27 16:02:47 +0700

    🍱 Add assets for NFT

---
+++ frontend/assets/index.ts
@@ -0,0 +1,5 @@
+import imageToken0 from "./listing0-token0-MonsterInc-LlamaMonster.png";
+import imageToken1 from "./listing1-token1-MonsterInc-LlamaMonster.png";
+import imageToken2 from "./unlisted-token2-MonsterInc-LlamaMonster.png";
+
+export const sampleNFTImages = [imageToken0, imageToken1, imageToken2];
