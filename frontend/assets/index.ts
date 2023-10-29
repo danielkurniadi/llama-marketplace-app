838125ea8528aa95098c2bb155542a26802c8a17
iqdf <daniel.thekurniadi@gmail.com>
2023-10-29 10:02:38 +0700

    Llama NFTs sample assets

--- frontend/assets/index.ts
+++ frontend/assets/index.ts
@@ -1,5 +1,27 @@
-import imageToken0 from "./listing0-token0-MonsterInc-LlamaMonster.png";
-import imageToken1 from "./listing1-token1-MonsterInc-LlamaMonster.png";
-import imageToken2 from "./unlisted-token2-MonsterInc-LlamaMonster.png";
+import imageToken0 from "./0-token000-LlamaMarket-LlamaSwag.png";
+import imageToken1 from "./1-token001-LlamaMarket-LlamaSwag.png";
+import imageToken2 from "./2-token002-LlamaMarket-LlamaSwag.png";
+import imageToken3 from "./3-token003-LlamaMarket-LlamaSwag.png";
+import imageToken4 from "./4-token004-LlamaMarket-LlamaSahara.png";
+import imageToken5 from "./5-token005-LlamaMarket-LlamaSahara.png";
+import imageToken6 from "./6-token006-LlamaMarket-LlamaSwag.png";
+import imageToken7 from "./7-token007-LlamaMarket-LlamaSwag.png";
+import unlistedImageToken8 from "./unlisted-token008-LlamaMarket-LlamaCyberPunk.png";
+import unlistedImageToken9 from "./unlisted-token009-LlamaMarket-LlamaCyberPunk.png";
+import unlistedImageToken10 from "./unlisted-token010-LlamaMarket-LlamaCyberPunk.png";
+import _marketLogoImage from "./market-logo.png";
 
-export const sampleNFTImages = [imageToken0, imageToken1, imageToken2];
+export const marketLogo = _marketLogoImage;
+export const sampleNFTImages = [
+  imageToken0,
+  imageToken1,
+  imageToken2,
+  imageToken3,
+  imageToken4,
+  imageToken5,
+  imageToken6,
+  imageToken7,
+  unlistedImageToken8,
+  unlistedImageToken9,
+  unlistedImageToken10,
+];
