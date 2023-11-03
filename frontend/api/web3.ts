c004180b7b3f51faf25eb51f460b3bc75f74ec53
iqdf <daniel.thekurniadi@gmail.com>
2023-11-03 23:46:14 +0800

    🔧 configure SECRET_KEY from envar or .env

--- frontend/api/web3.ts
+++ frontend/api/web3.ts
@@ -1,15 +1,15 @@
 import { NFT, ThirdwebSDK } from "@thirdweb-dev/sdk";
-import { ACTIVE_NETWORK } from "../const/addresses";
+import { ACTIVE_NETWORK, THIRDWEB_SECRET_KEY } from "../const/addresses";
 
 export async function fetchNFT(tokenId: string, collectionAddr: string): Promise<NFT> {
-  const sdk = new ThirdwebSDK(ACTIVE_NETWORK, { secretKey: process.env.THIRDWEB_SECRET_KEY });
+  const sdk = new ThirdwebSDK(ACTIVE_NETWORK, { secretKey: THIRDWEB_SECRET_KEY });
   const contract = await sdk.getContract(collectionAddr);
   const nft = await contract.erc721.get(tokenId);
   return nft;
 }
 
 export async function fetchNFTs(collectionAddr: string): Promise<NFT[]> {
-  const sdk = new ThirdwebSDK(ACTIVE_NETWORK, { secretKey: process.env.THIRDWEB_SECRET_KEY });
+  const sdk = new ThirdwebSDK(ACTIVE_NETWORK, { secretKey: THIRDWEB_SECRET_KEY });
   const contract = await sdk.getContract(collectionAddr);
   const nfts = await contract.erc721.getAll();
   return nfts;
@@ -19,7 +19,7 @@ export async function fetchNFTContractMetadata(
   tokenId: string,
   collectionAddr: string
 ): Promise<any> {
-  const sdk = new ThirdwebSDK(ACTIVE_NETWORK, { secretKey: process.env.THIRDWEB_SECRET_KEY });
+  const sdk = new ThirdwebSDK(ACTIVE_NETWORK, { secretKey: THIRDWEB_SECRET_KEY });
   const contract = await sdk.getContract(collectionAddr);
   let contractMetadata;
   try {
