14bd6e08d516b44f3f607e179d45c94219eea0bf
iqdf <daniel.thekurniadi@gmail.com>
2023-11-03 23:40:41 +0800

    🗑️ remove support for english auction listing (sell)

--- frontend/components/SaleInfo.tsx
+++ frontend/components/SaleInfo.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { NFT } from "@thirdweb-dev/sdk";
+import { FaEthereum } from "react-icons/fa";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
-import { Web3Button, useContract, useCreateAuctionListing } from "@thirdweb-dev/react";
+import {
+  Web3Button,
+  useContract,
+  useCreateDirectListing,
+  useValidDirectListings,
+} from "@thirdweb-dev/react";
 import { MARKETPLACE_ADDRESS, NFT_COLLECTION_ADDRESS } from "../const/addresses";
-import { Box, Input, Stack, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react";
-
-import { useCreateDirectListing } from "../hooks/listings";
+import {
+  Box,
+  Flex,
+  Icon,
+  Input,
+  Skeleton,
+  Stack,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 
 type Props = {
   nft: NFT;
@@ -20,21 +38,17 @@ type DirectFormData = {
   endDate: Date;
 };
 
-//Add for Auction
-type AuctionFormData = {
-  nftContractAddress: string;
-  tokenId: string;
-  startDate: Date;
-  endDate: Date;
-  floorPrice: string;
-  buyoutPrice: string;
-};
-
 export default function SaleInfo({ nft }: Props) {
   const router = useRouter();
   const { contract: marketplace } = useContract(MARKETPLACE_ADDRESS, "marketplace-v3");
   const { contract: nftCollection } = useContract(NFT_COLLECTION_ADDRESS);
   const { mutateAsync: createDirectListing } = useCreateDirectListing(marketplace);
+  const { data: directListings, isLoading } = useValidDirectListings(marketplace, {
+    tokenContract: NFT_COLLECTION_ADDRESS,
+    tokenId: nft.metadata.id,
+  });
+
+  const isListed = false; // TODO: check if this NFT is listed
 
   async function checkAndProvideApproval() {
     const hasApproval = await nftCollection?.call("isApprovedForAll", [
@@ -51,51 +65,12 @@ export default function SaleInfo({ nft }: Props) {
     return true;
   }
 
-  const { register: registerDirect, handleSubmit: onSubmitDirect } = useForm<DirectFormData>({
-    defaultValues: {
-      nftContractAddress: NFT_COLLECTION_ADDRESS,
-      tokenId: nft.metadata.id,
-      price: "0",
-      startDate: new Date(),
-      endDate: new Date(),
-    },
-  });
-
-  async function handleSubmissionDirect(data: DirectFormData) {
+  async function handleSubmitDirectListing(data: DirectFormData) {
     await checkAndProvideApproval();
     const txResult = await createDirectListing({
       assetContractAddress: data.nftContractAddress,
       tokenId: data.tokenId,
       pricePerToken: data.price,
-      startTimestamp: new Date(data.startDate),
-      endTimestamp: new Date(data.endDate),
-    });
-    
-    return txResult;
-  }
-
-  // Add for Auction
-  const { mutateAsync: createAuctionListing } = useCreateAuctionListing(marketplace);
-  const { register: registerAuction, handleSubmit: handleSubmitAuction } = useForm<AuctionFormData>(
-    {
-      defaultValues: {
-        nftContractAddress: NFT_COLLECTION_ADDRESS,
-        tokenId: nft.metadata.id,
-        startDate: new Date(),
-        endDate: new Date(),
-        floorPrice: "0",
-        buyoutPrice: "0",
-      },
-    }
-  );
-
-  async function handleSubmissionAuction(data: AuctionFormData) {
-    await checkAndProvideApproval();
-    const txResult = await createAuctionListing({
-      assetContractAddress: data.nftContractAddress,
-      tokenId: data.tokenId,
-      buyoutBidAmount: data.buyoutPrice,
-      minimumBidAmount: data.floorPrice,
       startTimestamp: new Date(data.startDate),
       endTimestamp: new Date(data.endDate),
     });
@@ -109,83 +84,106 @@ export default function SaleInfo({ nft }: Props) {
         <Tab>Direct</Tab>
         <Tab>Auction</Tab>
       </TabList>
-
       <TabPanels>
         <TabPanel>
-          <Stack spacing={8}>
-            <Box>
-              <Text>Listing starts on:</Text>
-              <Input
-                placeholder="Select Date and Time"
-                size="md"
-                type="datetime-local"
-                {...registerDirect("startDate")}
-              />
-              <Text mt={2}>Listing ends on:</Text>
-              <Input
-                placeholder="Select Date and Time"
-                size="md"
-                type="datetime-local"
-                {...registerDirect("endDate")}
-              />
-            </Box>
-            <Box>
-              <Text fontWeight={"bold"}>Price:</Text>
-              <Input placeholder="0" size="md" type="number" {...registerDirect("price")} />
-            </Box>
-            <Web3Button
-              contractAddress={MARKETPLACE_ADDRESS}
-              action={async () => {
-                await onSubmitDirect(handleSubmissionDirect)();
-              }}
+          {isLoading ? (
+            <Skeleton height={"300px"} width={"100%"} />
+          ) : !isListed ? (
+            <SaleCreateListingCard
+              nft={nft}
+              onSubmitAsync={handleSubmitDirectListing}
               onSuccess={(txResult) => {
                 router.push(`/token/${NFT_COLLECTION_ADDRESS}/${nft.metadata.id}`);
               }}
-            >
-              Create Direct Listing
-            </Web3Button>
-          </Stack>
+            />
+          ) : (
+            <VStack>
+              <Text fontSize={"md"}>You have listed this NFT!</Text>
+              <Web3Button
+                contractAddress={MARKETPLACE_ADDRESS}
+                action={async () => {
+                  console.log("Web3Button in [tokenId].tsx submitted but not doing anything...");
+                  // TODO: cancel listing
+                }}
+                onSuccess={(txResult) => {
+                  router.push(`/token/${NFT_COLLECTION_ADDRESS}/${nft.metadata.id}`);
+                }}
+                onError={(error) => {
+                  console.log("Web3Button in [tokenId].tsx error", error);
+                  router.push(`/token/${NFT_COLLECTION_ADDRESS}/${nft.metadata.id}`);
+                }}
+                theme="dark"
+                style={{ width: "100%", height: "50px" }}
+              >
+                Cancel Listing
+              </Web3Button>
+            </VStack>
+          )}
         </TabPanel>
         <TabPanel>
-          <Stack spacing={8}>
-            <Box>
-              <Text>Listing starts on:</Text>
-              <Input
-                placeholder="Select Date and Time"
-                size="md"
-                type="datetime-local"
-                {...registerAuction("startDate")}
-              />
-              <Text mt={2}>Listing ends on:</Text>
-              <Input
-                placeholder="Select Date and Time"
-                size="md"
-                type="datetime-local"
-                {...registerAuction("endDate")}
-              />
-            </Box>
-            <Box>
-              <Text fontWeight={"bold"}>Starting bid from:</Text>
-              <Input placeholder="0" size="md" type="number" {...registerAuction("floorPrice")} />
-            </Box>
-            <Box>
-              <Text fontWeight={"bold"}>Buyout price:</Text>
-              <Input placeholder="0" size="md" type="number" {...registerAuction("buyoutPrice")} />
-            </Box>
-            <Web3Button
-              contractAddress={MARKETPLACE_ADDRESS}
-              action={async () => {
-                return await handleSubmitAuction(handleSubmissionAuction)();
-              }}
-              onSuccess={(txResult) => {
-                router.push(`/token/${NFT_COLLECTION_ADDRESS}/${nft.metadata.id}`);
-              }}
-            >
-              Create Auction Listing
-            </Web3Button>
-          </Stack>
+          <Text fontSize={"md"}>Work in Progress. Auction feature coming soon.</Text>
         </TabPanel>
       </TabPanels>
     </Tabs>
   );
 }
+
+type SaleCreateListingProp = {
+  nft: NFT;
+  onSubmitAsync: (data: DirectFormData) => Promise<any>;
+  onSuccess: (txResult: any) => void;
+};
+
+const SaleCreateListingCard = ({ nft, onSubmitAsync, onSuccess }: SaleCreateListingProp) => {
+  const { register: registerForm, handleSubmit: onSubmitForm } = useForm<DirectFormData>({
+    defaultValues: {
+      nftContractAddress: NFT_COLLECTION_ADDRESS,
+      tokenId: nft.metadata.id,
+      price: "0",
+      startDate: new Date(),
+      endDate: new Date(),
+    },
+  });
+
+  return (
+    <Stack spacing={8}>
+      <Box>
+        <Text>Listing starts on:</Text>
+        <Input
+          placeholder="Select Date and Time"
+          size="md"
+          type="datetime-local"
+          {...registerForm("startDate")}
+        />
+        <Text mt={2}>Listing ends on:</Text>
+        <Input
+          placeholder="Select Date and Time"
+          size="md"
+          type="datetime-local"
+          {...registerForm("endDate")}
+        />
+      </Box>
+      <Box>
+        <Text fontWeight={"bold"}>Price:</Text>
+        <Flex direction={"row"}>
+          <Input placeholder="0" size="md" type="number" {...registerForm("price")} />
+          <Icon as={FaEthereum} ml={2} mt={2} w={6} h={6} />
+          <Text ml={2} mt={2} fontSize={"md"}>
+            ETH
+          </Text>
+        </Flex>
+      </Box>
+      <Web3Button
+        contractAddress={MARKETPLACE_ADDRESS}
+        action={async () => {
+          await onSubmitForm(onSubmitAsync)();
+        }}
+        onSuccess={(txResult) => {
+          onSuccess(txResult);
+        }}
+      >
+        Create Direct Listing
+      </Web3Button>
+    </Stack>
+  );
+};
